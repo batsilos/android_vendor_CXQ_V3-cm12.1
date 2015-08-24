a1bc9cd3f435eed9c380d35b8eb5f4a2e52973a3
Batsilos <batsilos@gmail.com>
2015-08-24 23:53:38 +0300

    Fist commit
    Start

---
+++ README.md
@@ -0,0 +1,1 @@
+# android_vendor_CXQ_V3
---
+++ V3-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CXQ/V3/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CXQ/V3/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CXQ/V3/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CXQ/V3/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CXQ/V3/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CXQ/V3/vendor,system/vendor)
